733060e80e698948e25f99420032264883a00483
Osaretin Igbinobaro <mail@osaretin.dev>
2023-09-11 19:50:23 +0100

    fix: change client dto to exclude active

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,42 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { Client } from './entities/client.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { RegisterClientDto } from './dto/register-client.dto';
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { Client } from "./entities/client.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { RegisterClientDto } from "./dto/register-client.dto";
 
 @Injectable()
 export class ClientService {
-  constructor(
-    @InjectRepository(Client)
-    private clientRepository: Repository<Client>,
-  ) {}
+	constructor(
+		@InjectRepository(Client)
+		private clientRepository: Repository<Client>
+	) {}
 
-  async create(registerClientDto: RegisterClientDto) {
-    const { firstName, lastName, email, password, role, active } =
-      registerClientDto;
+	async create(registerClientDto: RegisterClientDto) {
+		const { firstName, lastName, email, password, role } = registerClientDto;
 
-    const client = this.clientRepository.create({
-      firstName,
-      lastName,
-      email,
-      password,
-      role,
-      active,
-    });
+		const client = this.clientRepository.create({
+			firstName,
+			lastName,
+			email,
+			password,
+			role,
+		});
 
-    return await this.clientRepository.save(client);
-  }
+		return await this.clientRepository.save(client);
+	}
 
-  async findOne(id: number) {
-    if (!id) return null;
-    return await this.clientRepository.findOneBy({ id });
-  }
+	async findOne(id: number) {
+		if (!id) return null;
+		return await this.clientRepository.findOneBy({ id });
+	}
 
-  async findByEmail(email: string) {
-    return this.clientRepository.findOne({ where: { email } });
-  }
+	async findByEmail(email: string) {
+		return this.clientRepository.findOne({ where: { email } });
+	}
 
-  async find() {
-    return this.clientRepository.find({ relations: ['photos'] });
-  }
+	async find() {
+		return this.clientRepository.find({ relations: ["photos"] });
+	}
 }
