fd7735db1d3294538cb484427960e19fbd73045d
Osaretin Igbinobaro <mail@osaretin.dev>
2023-09-11 16:42:19 +0100

    feat: add client and auth services

--- authService/src/auth/auth.module.ts
+++ authService/src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Photo } from '../client/entities/photo.entity';
 import { Client } from '../client/entities/client.entity';
 import { ClientModule } from '../client/client.module';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Photo, Client]), ClientModule],
   providers: [],
-  controllers: [],
+  controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ authService/src/client/dto/register-client.dto.ts
@@ -0,0 +1,42 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+  Matches,
+} from 'class-validator';
+
+export class RegisterClientDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 25)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 25)
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Length(6, 50)
+  @Matches(/.*\d.*/, { message: 'Password must have atleast a number' })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  role: string;
+
+  @IsBoolean()
+  @IsOptional()
+  active: boolean;
+
+  @IsUrl()
+  @IsOptional()
+  avatar: string;
+}
