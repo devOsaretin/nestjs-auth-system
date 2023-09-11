ab2da53d0044b6d13fdf3344f3cad34fe6f06e1c
Osaretin Igbinobaro <mail@osaretin.dev>
2023-09-11 15:49:07 +0100

    chore: setup postgres client auth modules

---
+++ authService/src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('POSTGRES_HOST'),
+        port: configService.getOrThrow('POSTGRES_PORT'),
+        username: configService.getOrThrow('POSTGRES_USER'),
+        password: configService.getOrThrow('POSTGRES_PASSWORD'),
+        database: configService.getOrThrow('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: configService.getOrThrow('POSTGRES_SYNCHRONIZE'),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
