85f4ea5ba427b32fd677cefc23d135c04c8375b8
Osaretin Igbinobaro <mail@osaretin.dev>
2023-09-11 16:55:07 +0100

    feat: add auth services and providers

--- authService/src/auth/auth.module.ts
+++ authService/src/auth/auth.module.ts
@@ -1,13 +1,36 @@
 import { Module } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { Photo } from '../client/entities/photo.entity';
 import { Client } from '../client/entities/client.entity';
 import { ClientModule } from '../client/client.module';
-import { AuthController } from './auth.controller';
+import { ClientService } from '../client/client.service';
+import { AwsService } from '../aws/aws.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Photo, Client]), ClientModule],
-  providers: [],
+  imports: [
+    TypeOrmModule.forFeature([Photo, Client]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow('JWT_EXPIRES'),
+        },
+      }),
+    }),
+    ClientModule,
+  ],
+  providers: [AuthService, ClientService, AwsService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
