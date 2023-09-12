dfc7dc516a77cc3a5ada91009c8bf21c1b6764c2
Osaretin Igbinobaro <mail@osaretin.dev>
2023-09-12 10:45:01 +0100

    fix: add error handling

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,46 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import {
-  PutObjectCommand,
-  PutObjectCommandInput,
-  PutObjectCommandOutput,
-  S3Client,
-} from '@aws-sdk/client-s3';
+	PutObjectCommand,
+	PutObjectCommandInput,
+	PutObjectCommandOutput,
+	S3Client,
+} from "@aws-sdk/client-s3";
+import { HttpException } from "@nestjs/common";
 
 @Injectable()
 export class AwsService {
-  private readonly s3 = new S3Client({
-    region: this.configService.getOrThrow('AWS_REGION'),
-    credentials: {
-      accessKeyId: this.configService.getOrThrow('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: this.configService.getOrThrow('AWS_SECRET_ACCESS_KEY'),
-    },
-  });
-  constructor(private configService: ConfigService) {}
+	private readonly s3 = new S3Client({
+		region: this.configService.getOrThrow("AWS_REGION"),
+		credentials: {
+			accessKeyId: this.configService.getOrThrow("AWS_ACCESS_KEY_ID"),
+			secretAccessKey: this.configService.getOrThrow("AWS_SECRET_ACCESS_KEY"),
+		},
+	});
+	constructor(private configService: ConfigService) {}
 
-  async upload(fileName: string, file: Buffer) {
-    const bucketName = this.configService.get('AWS_BUCKET_NAME');
-    const region = this.configService.getOrThrow('AWS_REGION');
-    const key = `${Date.now()}${fileName}`;
-    const objectInput: PutObjectCommandInput = {
-      Bucket: bucketName,
-      Body: file,
-      Key: key,
-      ACL: 'public-read',
-    };
+	async upload(fileName: string, file: Buffer) {
+		const bucketName = this.configService.get("AWS_BUCKET_NAME");
+		const region = this.configService.getOrThrow("AWS_REGION");
+		const key = `${Date.now()}${fileName}`;
+		const objectInput: PutObjectCommandInput = {
+			Bucket: bucketName,
+			Body: file,
+			Key: key,
+			ACL: "public-read",
+		};
 
-    try {
-      const response: PutObjectCommandOutput = await this.s3.send(
-        new PutObjectCommand(objectInput),
-      );
-      if (response.$metadata.httpStatusCode === 200) {
-        return {
-          url: `https://${bucketName}.s3.${region}.amazonaws.com/${key}`,
-          name: fileName,
-        };
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+		try {
+			const response: PutObjectCommandOutput = await this.s3.send(
+				new PutObjectCommand(objectInput)
+			);
+			if (response.$metadata.httpStatusCode === 200) {
+				return {
+					url: `https://${bucketName}.s3.${region}.amazonaws.com/${key}`,
+					name: fileName,
+				};
+			}
+		} catch (error) {
+			throw new HttpException("File upload error", error);
+		}
+	}
 }
--- src/interceptors/fileupload.interceptor.ts
+++ src/interceptors/fileupload.interceptor.ts
@@ -1,39 +1,39 @@
 import {
-  CallHandler,
-  ExecutionContext,
-  HttpException,
-  HttpStatus,
-  NestInterceptor,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
+	CallHandler,
+	ExecutionContext,
+	HttpException,
+	HttpStatus,
+	NestInterceptor,
+} from "@nestjs/common";
+import { Observable } from "rxjs";
 
 export class FileUploadInterceptor implements NestInterceptor {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
-  ): Observable<any> | Promise<Observable<any>> {
-    const request = context.switchToHttp().getRequest();
+	intercept(
+		context: ExecutionContext,
+		next: CallHandler<any>
+	): Observable<any> | Promise<Observable<any>> {
+		const request = context.switchToHttp().getRequest();
 
-    if (!request.files || request.files.length < 4) {
-      throw new HttpException(
-        'At least 4 images are required',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
+		const validFileExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+		const isAllFilesImages = request.files.every(file => {
+			const fileExtension = file.originalname.split(".").pop().toLowerCase();
+			return validFileExtensions.includes(`.${fileExtension}`);
+		});
 
-    const validFileExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
-    const isAllFilesImages = request.files.every((file) => {
-      const fileExtension = file.originalname.split('.').pop().toLowerCase();
-      return validFileExtensions.includes(`.${fileExtension}`);
-    });
+		if (!isAllFilesImages) {
+			throw new HttpException(
+				"All uploaded files must be images",
+				HttpStatus.BAD_REQUEST
+			);
+		}
 
-    if (!isAllFilesImages) {
-      throw new HttpException(
-        'All uploaded files must be images',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
+		if (!request.files || request.files.length < 4) {
+			throw new HttpException(
+				"At least 4 images are required",
+				HttpStatus.BAD_REQUEST
+			);
+		}
 
-    return next.handle();
-  }
+		return next.handle();
+	}
 }
